87864e062e76c9002beb0a2c6f0fbd70577e7456
Stefka <skrachunova@appolica.com>
2022-04-30 15:12:35 +0300

    Add parallel email process
    - Add threads
    - Refresh table
    - Add separator function.

--- ParallelProgramming/Form1.Designer.cs
+++ ParallelProgramming/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.generate_button = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -44,26 +44,26 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // generate_button
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(12, 133);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 50);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Генерирай имейли";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.generate_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.generate_button.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.generate_button.Location = new System.Drawing.Point(83, 23);
+            this.generate_button.Name = "generate_button";
+            this.generate_button.Size = new System.Drawing.Size(175, 50);
+            this.generate_button.TabIndex = 1;
+            this.generate_button.Text = "Генерирай имейли";
+            this.generate_button.UseVisualStyleBackColor = true;
+            this.generate_button.Click += new System.EventHandler(this.Button1_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(669, 97);
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(728, 146);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(74, 24);
+            this.label5.Size = new System.Drawing.Size(88, 27);
             this.label5.TabIndex = 8;
             this.label5.Text = "Нишка 1";
             // 
@@ -71,10 +71,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(893, 93);
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(1071, 146);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(74, 24);
+            this.label6.Size = new System.Drawing.Size(88, 27);
             this.label6.TabIndex = 9;
             this.label6.Text = "Нишка 2";
             // 
@@ -82,10 +82,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(478, 534);
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(728, 413);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(74, 24);
+            this.label7.Size = new System.Drawing.Size(88, 27);
             this.label7.TabIndex = 10;
             this.label7.Text = "Нишка 3";
             // 
@@ -93,10 +93,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(653, 534);
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(1071, 413);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(74, 24);
+            this.label8.Size = new System.Drawing.Size(88, 27);
             this.label8.TabIndex = 11;
             this.label8.Text = "Нишка 4";
             // 
@@ -104,12 +104,13 @@
             // 
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(243, 132);
+            this.button2.Location = new System.Drawing.Point(380, 22);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(122, 51);
             this.button2.TabIndex = 12;
             this.button2.Text = "Обработка";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label9
             // 
@@ -132,57 +133,70 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 244);
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 88);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(353, 324);
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(565, 534);
             this.dataGridView1.TabIndex = 17;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
             // listBox3
             // 
+            this.listBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 20;
-            this.listBox3.Location = new System.Drawing.Point(459, 419);
+            this.listBox3.ItemHeight = 28;
+            this.listBox3.Location = new System.Drawing.Point(651, 443);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(150, 104);
+            this.listBox3.Size = new System.Drawing.Size(261, 32);
             this.listBox3.TabIndex = 20;
             // 
             // listBox4
             // 
+            this.listBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 20;
-            this.listBox4.Location = new System.Drawing.Point(637, 419);
+            this.listBox4.ItemHeight = 28;
+            this.listBox4.Location = new System.Drawing.Point(978, 443);
             this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(150, 104);
+            this.listBox4.Size = new System.Drawing.Size(261, 32);
             this.listBox4.TabIndex = 21;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(669, 124);
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(651, 177);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(150, 104);
+            this.listBox1.Size = new System.Drawing.Size(261, 32);
             this.listBox1.TabIndex = 22;
             // 
             // listBox2
             // 
+            this.listBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(873, 120);
+            this.listBox2.ItemHeight = 28;
+            this.listBox2.Location = new System.Drawing.Point(978, 177);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(150, 104);
+            this.listBox2.Size = new System.Drawing.Size(261, 32);
             this.listBox2.TabIndex = 23;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = global::ParallelProgramming.Properties.Resources.mb;
-            this.ClientSize = new System.Drawing.Size(1035, 599);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1284, 647);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.listBox4);
@@ -195,9 +209,10 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.generate_button);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -205,7 +220,7 @@
         }
 
         #endregion
-        private Button button1;
+        private Button generate_button;
         private Label label5;
         private Label label6;
         private Label label7;
--- ParallelProgramming/Form1.cs
+++ ParallelProgramming/Form1.cs
@@ -10,13 +10,23 @@ namespace ParallelProgramming
         }
 
         private void Button1_Click(object sender, EventArgs e)
+
         {
+            if (readEmails.Any((email) => !email.Processed)) {
+                System.Windows.Forms.MessageBox.Show("Моля първо обработете вече генерираните имейли.");
+                return;
+            }
+
             GenerateEmails();
+        
         }
 
         List<Emails> readEmails = new List<Emails>();
         public void GenerateEmails()
         {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            readEmails.Clear();
             string emailPattern = @"[a-z]{1,9}@((gmail)|(outlook)|(hotmail))\.com";
             string textPattern = "\\w{10,90}";
             RegExpDataGenerator emailGenerator = new RegExpDataGenerator(emailPattern);
@@ -27,9 +37,76 @@ namespace ParallelProgramming
                 readEmails.Add(new Emails(i.ToString(), emailGenerator.Next(), textGenerator.Next()));
             }
             dataGridView1.DataSource = readEmails;
+            dataGridView1.Refresh();
+            button2.Enabled = true;
         }
 
-        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(readEmails.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Моля първо генерирайте имейли.");
+                return;
+            }
+
+            ProcessEmails();
+            
+        }
+        public static List<List<T>> ChunkBy<T>(List<T> source, int chunkSize)
+        {
+            List<List<T>> result = source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+
+            if(result.Count > 4)
+            {
+                result[3].AddRange(result[4]);
+                result.Remove(result[4]);
+            }
+
+            return result;
+        }
+        public void ProcessEmails()
+        {
+            button2.Enabled = false;
+            int chunkSize = (int)Math.Ceiling((decimal)readEmails.Count / 4);
+            List<List<Emails>> chunkedEmails = ChunkBy(readEmails, chunkSize);
+
+            Thread threadOne = new Thread(() => SendEmails(chunkedEmails[0], listBox1));
+            Thread threadTwo = new Thread(() => SendEmails(chunkedEmails[1],listBox2));
+            Thread threadThree = new Thread(() => SendEmails(chunkedEmails[2],listBox3));
+            Thread threadFour = new Thread(() => SendEmails(chunkedEmails[3],listBox4));
+
+            threadOne.Start();
+            threadTwo.Start();
+            threadThree.Start();
+            threadFour.Start();
+        }
+
+        public void SendEmails(List<Emails> emails, ListBox box)
+        {
+            foreach (Emails email in emails)
+            {
+                this.Invoke(delegate () { box.Items.Add($"{email.Nummer} : {email.Email}"); } );
+                int random = new Random().Next(1,5);
+                Thread.Sleep(random * 1000);
+                email.Processed = true;
+                lock (dataGridView1)
+                {
+                    this.Invoke(delegate () { dataGridView1.Refresh(); });
+                }
+                this.Invoke(delegate () { box.Items.Clear(); });
+            }
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
